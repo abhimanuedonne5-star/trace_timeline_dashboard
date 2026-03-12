830f333a793caf4169614888cd882667e55f4d35
donne.abhimanue <donne.abhimanue@ril.com>
2026-03-12 13:14:48 +0000

    debugged

--- app.py
+++ app.py
@@ -697,4 +697,5 @@ def render_timeline(active):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8050, debug=False)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=False)
